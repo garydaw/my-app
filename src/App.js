c23b19961e99cc36f8e8b6556a7133fae9137d96
Gary <garydaw@ntlworld.com>
2023-10-29 15:02:59 +0000

    click api

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import { useEffect } from 'react';
 import  Player from './Components/Player.js';
 import { useState } from 'react';
 
-const myPlayerId = '832233694';
+//const myPlayerId = '832233694';
 
 export default function App() {
 
   const url = 'https://cors-anywhere.herokuapp.com/https://swgoh.gg/api/player/';
   const [playerUnits,setPlayerUnits] = useState([]);
 
-  const fetchInfo = () => {
-    return fetch(url + myPlayerId+'/')
-      .then((response) => response.json())
-      .then((data) => setPlayerUnits(data.units))
-  }
-
-
-  useEffect(() => {
-    fetchInfo();
-  }, []);
-
+  let getPlayerInfo = async () => {
+    try {
+        const data = await (await fetch(url + document.getElementById("playerId").value+'/')).json()
+        setPlayerUnits(data.units)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
   return (
     <div className="App">
+      <input type="text" id="playerId"></input>
+      <button onClick={getPlayerInfo}>Search</button>
       <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
       <center>
         {playerUnits.map((unit, index) => {
