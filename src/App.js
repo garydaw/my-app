4e5d7aa1aa8cd2c9d20906ec9ff554c75292fff6
Gary <garydaw@ntlworld.com>
2023-10-29 17:30:43 +0000

    filtering and sorting

--- src/App.js
+++ src/App.js
@@ -9,18 +9,57 @@ export default function App() {
 
   const url = 'https://cors-anywhere.herokuapp.com/https://swgoh.gg/api/player/';
   const [playerUnits,setPlayerUnits] = useState([]);
+  const [allUnits,setAllPlayerUnits] = useState([]);
+  const [combatType,setCombatType] = useState("units");
 
   let getPlayerInfo = async () => {
     try {
-        const data = await (await fetch(url + document.getElementById("playerId").value+'/')).json()
-        let units = data.units;
 
-        units.sort((a, b) => (a.data.power < b.data.power) ? 1 : -1)
-        setPlayerUnits(units)
+        //get data
+        const data = await (await fetch(url + document.getElementById("playerId").value+'/')).json()
+
+        setAllPlayerUnits(data.units.sort((a, b) => (a.data.power < b.data.power) ? 1 : -1));
+
+        showUnits();
+
+        
+
     } catch (err) {
         console.log(err.message)
     }
-}
+  }
+
+  function showUnits(){
+
+    if(combatType == "units" ){
+      setPlayerUnits(allUnits.filter(checkUnits));
+    } else {
+      setPlayerUnits(allUnits.filter(checkShips));
+    }
+    
+    function checkUnits(unit) {
+      return  unit.data.combat_type == 1;
+    }
+
+    function checkShips(unit) {
+      return  unit.data.combat_type == 2;
+    }
+
+  }
+
+  let checkCombatType = async () => {
+    if(document.getElementById("unitsRadio").checked && combatType != "units"){
+      setCombatType("units");
+      showUnits();
+    } else if (document.getElementById("shipsRadio").checked && combatType != "ships"){
+      setCombatType("ships");
+      showUnits();
+    }
+  }
+
+  let doNothing = async () => {
+    //nothing!
+  }
 
   return (
     <div className="container">
@@ -33,14 +72,24 @@ export default function App() {
             aria-label="Player ID"
             aria-describedby="Player ID"
             id="playerId"
-            value={myPlayerId}></input>
+            value={myPlayerId}
+            ></input>
         
         <button className="form-control btn btn-primary" onClick={getPlayerInfo}>Search</button>
       </div>
+      
+      <div className="btn-group mb-3" role="group" aria-label="Combat Selection">
+        <input type="radio" className="btn-check" name="combatType" id="unitsRadio" autoComplete="off" onChange={doNothing} onClick={checkCombatType}></input>
+        <label className="btn btn-outline-primary" for="unitsRadio">Units</label>
+
+        <input type="radio" className="btn-check" name="combatType" id="shipsRadio" autoComplete="off" onClick={checkCombatType}></input>
+        <label className="btn btn-outline-primary" for="shipsRadio">Ships</label>
+      </div>
+
       <div className="row row-cols-3">
         {playerUnits.map((unit, index) => {
           return (
-              <UnitOverview unit={unit.data}></UnitOverview>
+              <UnitOverview unit={unit.data} key={unit.data.base_id}></UnitOverview>
           );
         })}
       </div>
